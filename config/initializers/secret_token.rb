185d5d11c3b797869bb44e4bce8ce5d3b7f5b060
Tennyson Hinds <StlTenny@gmail.com>
2012-07-10 19:05:55 -0400

    Initializing rails repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lj::Application.config.secret_token = 'd4c9dd9083a79469aa99cd441643ff8384b86e361228c1d31c36b575f4fed1004f2d2c9c5e9e5680c6164d3dea454d78bb4d899d87c44252d8d7695ddd01a25e'
