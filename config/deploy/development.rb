a71d6a866ae447132c30a8aae78002000238a291
Tennyson Hinds <StlTenny@gmail.com>
2012-09-05 19:04:07 -0400

    Changing Development env

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,4 +1,4 @@
-set :location, dev.loopjoy.com
+set :location, "dev.loopjoy.com"
 role :web, location
 role :app, location
 role :db,  location, :primary => true
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :location, loopjoy.com
+set :location, "loopjoy.com"
 role :web, location
 role :app, location
 role :db,  location, :primary => true
